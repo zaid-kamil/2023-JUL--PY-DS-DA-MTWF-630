24ac1086906f82aca80c21da2c1b1781f353ff69
zaid kamil <xaidmetamorphos@gmail.com>
2023-08-19 13:09:56 +0530

    numpy

--- game.py
+++ game.py
@@ -1,87 +1,112 @@
 import pgzrun
 from random import randint
+from dataclasses import dataclass
 
-HEIGHT = 700
-WIDTH = 1200
-w, h = WIDTH, HEIGHT
+# create a data class to store game configuration
+@dataclass
+class GameConfig:
+    # define the fields using type annotations and default values
+    HEIGHT: int = 700
+    WIDTH: int = 1024
+    BG_WIDTH: int = 1000
+    SCORE_LIMIT: int = 100
+    FONT_SIZE: int = 50
+
+    # optionally, add some methods or properties if needed
+    def screen_size(self) -> tuple:
+        # return a tuple of screen width and height
+        return (self.WIDTH, self.HEIGHT)
+
+# create an instance of GameConfig
+config = GameConfig()
+WIDTH = config.WIDTH
+HEIGHT = config.HEIGHT
+
+screen_width, screen_height = config.screen_size()
 music.play('remix')
 
-p = Actor('ironman', (w//2, h//2))
-c = Actor('coin', (w//2, h-100))
-
-e_list = []
+player = Actor('ironman', (screen_width//2, screen_height//2))
+coin = Actor('coin', (screen_width//2, screen_height-100))
+backgrounds = []
 for i in range(10):
-    e_list.append(Actor('alien', (randint(0, w), randint(0, h))))
+    backgrounds.append(Actor('bg', (i*config.BG_WIDTH, screen_height//2)))
 
-block1 = Rect((0, 0), (w, 40))
+# use a list comprehension to create the enemy list
+enemies = [Actor('alien', (randint(0, screen_width), randint(0, screen_height))) for i in range(10)]
+
+particle = Rect((0, 0), (5, 5))    
+
+blue_rect = Rect((0, 0), (screen_width, 40))
 score = 0
 game_state = -1
+game_over = False # use a variable to store the game state of 1 or 2
+
+# use a function to generate a random position
+def randomize_position():
+    return (randint(100, screen_width-100), randint(100, screen_height-100))
+
+def scroll_bacground():
+    for bg in backgrounds:
+        bg.x -= 1
+        if bg.x < -config.BG_WIDTH:
+            bg.x = screen_width
+
 
 def draw_game_level():
     screen.fill('black')
+    for bg in backgrounds:
+        bg.draw()
     # name of the game
-    screen.draw.text('Ironman Game', (w//2, 10),
-                        color='white', fontsize=50, centerx=w//2)
-    screen.draw.filled_rect(block1, 'blue')
+    screen.draw.text('Ironman Game', (screen_width//2, 10),
+                        color='white', fontsize=config.FONT_SIZE, centerx=screen_width//2)
+    screen.draw.filled_rect(blue_rect, 'blue')
     screen.draw.text(f'Score: {score}', (10,10),
                         color='white')
-    c.draw()
-    p.draw()
-    for e in e_list:
+    coin.draw()
+    player.draw()
+    for e in enemies:
+        # randomize the alien size
         e.draw()
 
 def draw():
     if game_state == -1:
         screen.fill('yellow')
-        screen.draw.text(f'Press Space to Start', color='black', fontsize=50, centery=h//2, centerx=w//2)
+        screen.draw.text(f'Press Space to Start', color='black', fontsize=config.FONT_SIZE, centery=screen_height//2, centerx=screen_width//2)
     if game_state == 0:
         draw_game_level()
-    elif game_state == 1:
+    elif game_state == 1 or game_state == 2:
         screen.fill('black')
-        screen.draw.text(f'Game Over', color='red', fontsize=100, centery=h//2, centerx=w//2)
-    elif game_state == 2:
-        screen.fill('black')
-        screen.draw.text(f'You Win',color='green', fontsize=100, centery=h//2, centerx=w//2)
+        # use the game_over variable to display the appropriate message
+        screen.draw.text(f'Game Over' if game_over else f'You Win', color='red' if game_over else 'green', fontsize=config.FONT_SIZE, centery=screen_height//2, centerx=screen_width//2)
 
 def player_update():
-    if keyboard.left:
-        p.x -= 10
-        p.angle = 20
-    elif keyboard.right:
-        p.x += 10
-        p.angle = -20
-    elif keyboard.up:
-        p.y -= 10
-    elif keyboard.down:
-        p.y += 10
-    else:
-        p.angle = 0
+    if player.x > screen_width:
+        player.x = 0
+    if player.x < 0:
+        player.x = screen_width
+    if player.y > screen_height:
+        player.y = 0
+    if player.y < 0:
+        player.y = screen_height
 
-    # loop around the screen
-
-    if p.x > w:
-        p.x = 0
-    if p.x < 0:
-        p.x = w
-    if p.y > h:
-        p.y = 0
-    if p.y < 0:
-        p.y = h
-
+def on_mouse_move(pos):
+    # animate the player movement
+    animate(player, pos=pos, duration=0.3)
+    
 def enemy_update():
-    for e in e_list:
-        s = randint(1, 4)
-        if c.x > e.x:
-            e.x += s
-        if c.x < e.x:
-            e.x -= s
-        if c.y > e.y:
-            e.y += s
-        if c.y < e.y:
-            e.y -= s
+    for e in enemies:
+        speed = randint(1, 4)
+        if coin.x > e.x:
+            e.x += speed
+        if coin.x < e.x:
+            e.x -= speed
+        if coin.y > e.y:
+            e.y += speed
+        if coin.y < e.y:
+            e.y -= speed
     # keep them from colliding together
-    for e in e_list:
-        for e2 in e_list:
+    for e in enemies:
+        for e2 in enemies:
             if e.colliderect(e2):
                 if e.x > e2.x:
                     e.x += 5
@@ -93,29 +118,32 @@ def enemy_update():
                     e.y -= 5
 
 def score_update():
-    global score, game_state
-    if p.colliderect(c):
+    global score, game_state, game_over
+    if player.colliderect(coin):
         score += 10
-        c.x = randint(0, w)
-        c.y = randint(0, h)
+        # use the randomize_position function to set the coin position
+        coin.pos = randomize_position()
         sounds.action.play()
     
-    for e in e_list:
-        if e.colliderect(c):
+    for e in enemies:
+        if e.colliderect(coin):
             score -= 5
-            c.x = randint(0, w)
-            c.y = randint(0, h)
+            # use the randomize_position function to set the coin position
+            coin.pos = randomize_position()
             sounds.action.play()
-    if score <= -100:
+    if score <= -config.SCORE_LIMIT:
         game_state = 1 # game over
-    if score >= 100:
+        game_over = True # set the game_over variable to True
+    if score >= config.SCORE_LIMIT:
         game_state = 2 # you win
+        game_over = False # set the game_over variable to False
         
 
 
 def update():
     global game_state
     if game_state == 0:
+        scroll_bacground()
         enemy_update()
         player_update()
         score_update()
